ac05603072d41a41248e01a63c4b0b326c992690
Sharafat Ibn Mollah Mosharraf <sharafat_8271@yahoo.co.uk>
2024-04-10 19:14:43 +0600

    Make compatible with Monolog v3

--- src/Monolog/Formatter/LogdnaFormatter.php
+++ src/Monolog/Formatter/LogdnaFormatter.php
@@ -25,7 +25,7 @@ class LogdnaFormatter extends \Monolog\Formatter\JsonFormatter {
         parent::__construct($batchMode, $appendNewline);
     }
 
-    public function format(array $record): string {
+    public function format($record): string {
         $date = new \DateTime();
 
         $json = [
